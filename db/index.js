05387ba7eff61ac6841c22c545a024c77d2c60e0
Emir Salazar <emiralexisl@gmail.com>
2018-09-30 10:21:28 -0500

    Creando version final de la clase.

--- db/index.js
+++ db/index.js
@@ -2,6 +2,16 @@ const mongo = require("./connect");
 const { DB_NAME } = require("./config");
 
 module.exports = {
+    getFilms: function(){
+        const db = mongo.instance().db(DB_NAME);
+        const resp = db.collection("films").find().toArray();
+        return resp;
+    },
+    getFilmByID: function(_id){
+        const db = mongo.instance().db(DB_NAME);
+        const resp = db.collection("films").find({ _id }).toArray();
+        return resp;
+    },
     postFilm: function(film) {
         const db = mongo.instance().db(DB_NAME);
         const resp = db.collection("films").insertOne(film);
--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,18 @@
 const API_PATH = '/api';
 const films = require("../resources/films.json");
 const mongo = require("../db/connect");
-const { postFilm } = require("../db");
+const { postFilm, getFilms, getFilmByID } = require("../db");
 
 module.exports = (app) => {
-    app.get(`${API_PATH}/films`, (req, res)=>{
-        res.json(films);
+    app.get(`${API_PATH}/films`, async (req, res)=>{
+        const resp = await getFilms();
+        return res.json(resp);
     });
 
-    app.get(`${API_PATH}/films/:id`, (req, res)=>{
+    app.get(`${API_PATH}/films/:id`, async (req, res)=>{
         const id = req.params.id;
-        const film = films.filter(film=>film.id === id);
-        res.json(film);    
+        const resp = await getFilmByID(id);
+        return res.json(resp);
     });
 
     app.post(`${API_PATH}/film`, async (req, res)=>{
