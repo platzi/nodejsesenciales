56f179995d2d272e41fe51679662d8b12f1c908c
Emir Salazar <emiralexisl@gmail.com>
2018-10-03 23:07:09 -0500

    Creando version final de la clase.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+const mongo = require("./db/connect");
+
 app.use( logger("dev") );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: false }) );
@@ -12,7 +14,23 @@ require("./routes/views")(app);
 require("./routes/api")(app);
 require("./routes/special")(app);
 
-console.log("Iniciando Express.js");
-app.listen(3000, ()=>{
-    console.log("Express ha iniciado correctamente!");
-});
+async function initMongo(){
+    const db = await mongo.connect();
+    if(db) { initExpress(); }
+}
+
+function initExpress() {
+    console.log('Iniciando Express');
+    app.listen(3000, ()=>{
+        console.log("Express ha iniciado correctamente!");
+        process.on("SIGINT", closeApp);
+        process.on("SIGTERM", closeApp);
+    });
+}
+
+function closeApp(){
+    mongo.disconnect()
+        .then(()=>process.exit(0));
+}
+
+initMongo();
