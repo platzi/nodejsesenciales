7213a5827a0e6a7229142fac8a2c4d6cd04dd1b3
Emir Salazar <emiralexisl@gmail.com>
2018-10-04 03:45:42 -0500

    Creando base para la clase.

---
+++ index.js
@@ -0,0 +1,4 @@
+const ops = require("./math/ops");
+
+const suma = ops.sum(5,6);
+const producto = ops.prod(5,6);
---
+++ math/ops.js
@@ -0,0 +1,32 @@
+/**
+ * Funcion para sumar dos numeros
+ * @param {number} a 
+ * @param {number} b 
+ */
+function sum(a, b){
+    return a + b;
+}
+
+/**
+ * Funcion para restar dos numeros
+ * @param {number} a 
+ * @param {number} b 
+ */
+function diff(a, b){
+    return a + b;
+}
+
+function prod(a, b){
+    return a + b;
+}
+
+function div(a, b){
+    return a + b;
+}
+
+module.exports = {
+    sum,
+    diff,
+    prod,
+    div
+}
